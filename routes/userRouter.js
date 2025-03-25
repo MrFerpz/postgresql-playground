45465e79574c26947d5355f3fbb86b3c0d7d8575
Fred <leferpz@gmail.com>
2025-03-25 08:26:15 -0400

    Basic express project set-up

---
+++ routes/userRouter.js
@@ -0,0 +1,8 @@
+const Router = require('express');
+const userRouter = Router();
+
+userRouter.get("/", (req, res) => console.log("usernames will be logged here -wip"));
+userRouter.get("/new", (req, res) => res.render("form"));
+userRouter.post("/new", (req, res) => console.log("username to be saved: ", req.body.username));
+
+exports.module(userRouter);
